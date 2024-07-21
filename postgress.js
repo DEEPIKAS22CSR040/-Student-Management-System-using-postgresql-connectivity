da808faef3068d3152f54d589496137a35694351
DEEPIKAS22CSR040 <127317696+DEEPIKAS22CSR040@users.noreply.github.com>
2024-07-21 20:20:33 +0530

    Modified port number

--- postgress.js
+++ postgress.js
@@ -272,6 +272,6 @@ app.get('/report', async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(9000, () => {
+    console.log('Server is running on port 9000');
 });
